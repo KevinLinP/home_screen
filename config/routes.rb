6aafc79286a07d761f8e3067f49c59a0d4689fa6
Kevin Lin <kevin.lin.p@gmail.com>
2017-10-11 09:57:34 -0700

    Trying out AppCache

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   rack_offline = Rack::Offline.configure :cache_interval => 7.days.to_i do
     cache ActionController::Base.helpers.asset_path('application.css')
     cache ActionController::Base.helpers.asset_path('application.js')
-    cache ActionController::Base.helpers.asset_pack_path('home_screen.js')
+    cache Webpacker.manifest.lookup("home_screen.js")
     # cache other assets
     network "/"
   end
