ec13dccafdb3180f5ecaca35cd8cf28b215e8b0b
Kevin Lin <kevin.lin.p@gmail.com>
2017-10-11 23:36:29 -0700

    adding LinksForm

--- app/javascript/packs/links.jsx
+++ app/javascript/packs/links.jsx
@@ -4,6 +4,17 @@ import { connect, PromiseState } from 'react-refetch'
 import _ from 'underscore'
 
 class Links extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: true
+    }
+  }
+
+  handleCreate(props) {
+    alert(props);
+  }
+
   render() {
     const { linksFetch } = this.props;
 
@@ -15,18 +26,71 @@ class Links extends React.Component {
     links = _.sortBy(links, 'position');
     links = _.map(links, (link) => {
       return (
-        <a className="link" key={link.id} href={link.url}>
-          <img className="link-image" src={link.image} />
-          <div className="link-name">{link.name}</div>
-        </a>
+        <Link key={link.id} {...link}/>
       );
     });
+
+    let form = null;
+    if (this.state.isEditing) {
+      form = (<LinksForm onCreate={this.handleCreate.bind(this)} />);
+    }
 
     return (
       <div>
         <div className="links-heading">Favorites</div>
         <div className="links">{links}</div>
+        {form}
       </div>
+    );
+  }
+}
+
+function Link(props) {
+  return (
+    <a className="link" href={props.url}>
+      <img className="link-image" src={props.image} />
+      <div className="link-name">{props.name}</div>
+    </a>
+  );
+}
+
+// https://reactjs.org/docs/forms
+class LinksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.emptyState();
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onCreate(this.state)
+
+    this.setState(this.emptyState());
+  }
+
+  emptyState() {
+    return {
+      name: '',
+      url: '',
+      image: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form className="links-form" onSubmit={this.handleSubmit.bind(this)}>
+        <input type="text" placeholder="name" name="name" value={this.state.name} onChange={this.handleChange.bind(this)} />
+        <input type="text" placeholder="url" name="url" value={this.state.url} onChange={this.handleChange.bind(this)} />
+        <input type="text" placeholder="icon url" name="image" value={this.state.image} onChange={this.handleChange.bind(this)} />
+        <button type="submit">+</button>
+      </form>
     );
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+
   root 'home_screen#show'
 
   resource :nicehash, only: :show
   resources :links, only: :index
 
-  rack_offline = Rack::Offline.configure :cache_interval => 7.days.to_i do
-    cache ActionController::Base.helpers.asset_path('application.css')
-    cache ActionController::Base.helpers.asset_path('application.js')
-    cache Webpacker.manifest.lookup("home_screen.js")
-    # cache other assets
-    network "/"
+  if Rails.env.production?
+    rack_offline = Rack::Offline.configure :cache_interval => 7.days.to_i do
+      cache ActionController::Base.helpers.asset_path('application.css')
+      cache ActionController::Base.helpers.asset_path('application.js')
+      cache Webpacker.manifest.lookup("home_screen.js")
+
+      network "/"
+    end
+    get '/application.manifest' => rack_offline
   end
-  get '/application.manifest' => rack_offline
+
 end
