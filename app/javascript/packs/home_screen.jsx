5412aa4f473aa06b56289a093646cbc88523a150
Kevin Lin <kevin.lin.p@gmail.com>
2017-10-31 21:21:51 -0700

    basic Links data hooked up to graphql

--- app/javascript/packs/home_screen.jsx
+++ app/javascript/packs/home_screen.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import DaylightInfo from './daylight-info'
 import Clock from './clock'
 import Weather from './weather'
 //import Todo from './todo'
 import NicehashStats from './nicehash-stats'
-import Links from './links'
+import LinksListWithData from './links'
 import RedditTldr from './reddit-tldr'
 import UpcomingEvents from './upcoming-events'
 
 document.addEventListener('DOMContentLoaded', () => {
+  const client = new ApolloClient({
+    link: new HttpLink(),
+    cache: new InMemoryCache()
+  });
+
   ReactDOM.render(<DaylightInfo/>, document.getElementById('react-daylight-info'));
   ReactDOM.render(<Clock/>, document.getElementById('react-clock'));
   ReactDOM.render(<Weather/>, document.getElementById('react-weather'));
-  //ReactDOM.render(<Todo items={document.data.todo} />, document.getElementById('react-todo'));
-  ReactDOM.render(<NicehashStats/>, document.getElementById('react-nicehash-stats'));
-  ReactDOM.render(<Links/>, document.getElementById('react-links'));
+  ReactDOM.render((<NicehashStats/>), document.getElementById('react-nicehash-stats'));
+  ReactDOM.render((
+    <ApolloProvider client={client}>
+      <LinksListWithData/>
+    </ApolloProvider>
+  ), document.getElementById('react-links'));
   ReactDOM.render(<RedditTldr/>, document.getElementById('react-reddit-tldr'));
   ReactDOM.render(<UpcomingEvents/>, document.getElementById('react-upcoming-events'));
 });
--- app/javascript/packs/links.jsx
+++ app/javascript/packs/links.jsx
@@ -1,213 +1,42 @@
 import React from 'react'
-import restful, { fetchBackend } from 'restful.js';
 import _ from 'underscore'
-import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
-import lscache from 'lscache'
 
-import LinksForm from './links-form'
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-// TODO: figure out how to break up this file
-
-export default class Links extends React.Component {
+class LinksList extends React.Component {
   constructor(props) {
     super(props);
-
-    let links = lscache.get('links');
-    if (!links) {
-      links = [];
-    }
-
-    this.state = {
-      links: links,
-      showEditControls: false,
-      form: this.emptyFormProps()
-    };
-
-    this.handleIndexResponse = this.handleIndexResponse.bind(this);
-    this.handleEditToggle = this.handleEditToggle.bind(this);
-    this.handleLinkEditButton = this.handleLinkEditButton.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleFormChange = this.handleFormChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    this.handleSortEnd = this.handleSortEnd.bind(this);
-    this.resetForm = this.resetForm.bind(this);
-
-    const api = restful(window.location.protocol + '//' + window.location.host, fetchBackend(fetch)); // TODO: hacky
-    this.collection = api.all('links'); 
-
-    this.collection.getAll().then(this.handleIndexResponse);
-  }
-
-  handleIndexResponse(response) {
-    let links = _.map(response.body(), (entity) => {
-      let data = entity.data();
-      data.index = data.position;
-      delete data.position;
-
-      return data;
-    });
-    links = _.sortBy(links, 'index');
-
-    this.setState({
-      links: links,
-    });
-
-    lscache.set('links', links);
-  }
-
-  resetForm() {
-    this.setState({
-      form: this.emptyFormProps()
-    });
-  }
-
-  emptyFormProps() {
-    return {
-      name: '',
-      url: '',
-      image: ''
-    };
-  }
-
-  handleFormSubmit() {
-    let data = this.state.form;
-    let promise;
-
-    if (data.id) {
-      const id = data.id;
-      data = _.pick(data, 'name', 'url', 'image');
-
-      promise = this.collection.patch(id, data);
-    } else {
-      promise = this.collection.post(data);
-    }
-
-    promise.then(this.handleIndexResponse).then(this.resetForm);
-  }
-
-  handleLinkEditButton(link) {
-    if (link.id == this.state.form.id) {
-      this.resetForm();
-    } else {
-      this.setState({form: link});
-    }
-  }
-
-  handleDelete(link) {
-    this.collection.delete(link.id).then(this.handleIndexResponse);
-  }
-
-  handleFormChange(field, value) {
-    this.setState({
-      form: Object.assign({}, this.state.form, {[field]: value})
-    });
-  }
-
-  handleEditToggle() {
-    var newState = !this.state.showEditControls;
-    this.setState({
-      showEditControls: newState
-    });
-
-    if (!newState && this.state.form.id) {
-      this.resetForm();
-    }
-  }
-
-  handleSortEnd({oldIndex, newIndex}) {
-    if (newIndex == oldIndex) {
-      return;
-    }
-
-    const link = this.state.links[oldIndex];
-
-    this.setState({
-      links: arrayMove(this.state.links, oldIndex, newIndex) // doesn't update .index, only returns new array with reordered items
-    });
-
-    this.collection.patch(link.id, {position: newIndex}).then(this.handleIndexResponse);
   }
 
   render() {
-    let form = null;
-    let editButtonClass = 'homescreen-header-edit-toggle';
-    if (this.state.showEditControls) {
-      form = (<LinksForm onSubmit={this.handleFormSubmit} onChange={this.handleFormChange} {...this.state.form} />);
-      editButtonClass += ' active';
+    let links = this.props.data.links;
+    if (!links) {
+      return null;
     }
 
-    const sortableContainerSettings = {
-      axis: 'xy'
-    };
-    const linksListProps = {
-      links: this.state.links,
-      showEditControls: this.state.showEditControls,
-      onLinkEditButton: this.handleLinkEditButton,
-      onDelete: this.handleDelete,
-      onSortEnd: this.handleSortEnd,
-      ...sortableContainerSettings
-    };
+    links = _.map(links, (link) => {
+      return (<li key={link.id}>{link.name}</li>);
+    });
 
     return (
-      <div>
-        <div className="homescreen-header">
-          Favorites
-          <a href="javascript:void(0);" className={editButtonClass} onClick={this.handleEditToggle}>edit</a>
-        </div>
-        <LinksList {...linksListProps}/>
-        {form}
-      </div>
+      <ul>
+        {links}
+      </ul>
     );
   }
 }
 
-const LinksList = SortableContainer((props) => {
-  const links = _.map(props.links, (link) => {
-    const sortableElementProps = {
-      disabled: !props.showEditControls
-    };
-
-    const linkProps = {
-      ...link,
-      key: link.id,
-      showEditControls: props.showEditControls,
-      onEdit: () => {props.onLinkEditButton(link);},
-      onDelete: () => {props.onDelete(link);},
-      ...sortableElementProps
-    };
-
-    return (
-      <Link {...linkProps} />
-    );
-  });
-
-  return (<div className="links">{links}</div>);
-});
-
-const Link = SortableElement((props) => {
-  let editControls = null;
-  if (props.showEditControls) {
-    editControls = (
-      <div className="link-edit-controls">
-        <button className="link-edit-button" onClick={props.onEdit}>
-          <i className="fa fa-pencil-square-o"></i>
-        </button>
-        <button className="link-delete-button" onClick={props.onDelete}>
-          <i className="fa fa-trash-o"></i>
-        </button>
-      </div>
-    );
+const linksListQuery = gql`
+query {
+  links {
+    id
+    name
+    url
+    image
   }
+}`;
 
-  return (
-    <div className="link">
-      <a className="link-link" href={props.url}>
-        <div className="link-image-container">
-          <img className="link-image" src={props.image} />
-        </div>
-        <div className="link-name">{props.name}</div>
-      </a>
-      {editControls}
-    </div>
-  );
-});
+const LinksListWithData = graphql(linksListQuery)(LinksList);
+
+export default LinksListWithData;
